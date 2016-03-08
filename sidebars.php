61abd5cca0a33db9a31e087908857a9de083b455
Damir Calusic <damir@damircalusic.com>
2016-03-08 10:44:45 +0100

    Create sidebars.php

---
+++ sidebars.php
@@ -0,0 +1,15 @@
+<?php
+if ( !defined('ABSPATH')){ exit; }
+
+/**
+* Register sidebars
+*/
+register_sidebar( array(
+	'name'          => __('Sidebar 1', 'superkreativ'),
+	'id'            => 'sidebar-1',
+	'description'   => '',
+	'before_widget' => '',
+	'after_widget'  => '',
+	'before_title'  => '',
+	'after_title'   => '',
+) );
