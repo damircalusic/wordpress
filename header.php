fab07e98bdecf193acda7737a60c5be543cad3c8
Damir Calusic <damir@damircalusic.com>
2016-10-13 13:08:11 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,63 @@
+<?php global $wp_query; ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<link rel="dns-prefetch" href="//googlesyndication.com">
+<link rel="dns-prefetch" href="//google-analytics.com">
+<link rel="dns-prefetch" href="//googleapis.com">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="HandheldFriendly" content="true">
+<meta name="apple-mobile-web-app-title" content="<?php bloginfo('sitename'); ?>">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="application-name" content="<?php bloginfo('sitename'); ?>">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="msapplication-config" content="/browserconfig.xml" />
+<meta name="theme-color" content="#ffffff">
+<meta name="geo.region" content="SE">
+<meta name="geo.placename" content="Arvika">
+<meta name="geo.position" content="59.665514;12.57853">
+<meta name="ICBM" content="59.665514, 12.57853">
+<meta name="DC.creator" content="<?php bloginfo('sitename'); ?>" />
+<meta name="DC.publisher" content="<?php bloginfo('sitename'); ?>" />
+<meta name="DC.title" content="<?php bloginfo('sitename'); ?>" />
+<meta name="DC.coverage" content="Sverige" />
+<meta name="DC.type" content="Text" />
+<meta name="DC.format" content="text/html" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="icon" type="image/png" href="/android-chrome-512x512.png" sizes="512x512">
+<link rel="icon" type="image/png" href="/android-chrome-384x384.png" sizes="384x384">
+<link rel="icon" type="image/png" href="/android-chrome-256x256.png" sizes="256x256">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/android-chrome-144x144.png" sizes="144x144">
+<link rel="icon" type="image/png" href="/android-chrome-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-72x72.png" sizes="72x72">
+<link rel="icon" type="image/png" href="/android-chrome-48x48.png" sizes="48x48">
+<link rel="icon" type="image/png" href="/android-chrome-36x36.png" sizes="36x36">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,600" media="not all" onload="if(media!='all')media='all'">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+<?php wp_head(); ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body <?php body_class(); ?> itemscope itemtype="https://schema.org/LocalBusiness">
+<div id="page" class="site">
+<header id="masthead" class="site-header">
+<nav id="site-navigation" class="main-navigation"><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'huvud-meny-top', 'menu_class' => 'huvud-meny-top', 'container'  => '' ) ); ?></nav>
+</header>
+<div id="content" class="site-content">
