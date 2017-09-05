5886859cbbe3a8e3c298cff831758413e3fd324b
Damir Calusic <damir@damircalusic.com>
2017-09-05 17:59:35 +0200

    Update sidebars.php

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -5,7 +5,7 @@ if ( !defined('ABSPATH')){ exit; }
 * Register sidebars
 */
 register_sidebar( array(
-	'name'          => __('Sidebar 1', 'superkreativ'),
+	'name'          => __('Sidebar 1', 'wktheme'),
 	'id'            => 'sidebar-1',
 	'description'   => '',
 	'before_widget' => '',
