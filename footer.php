e83e55e0087cc1ac2bec8d204828f3297d143828
Damir Calusic <damir@damircalusic.com>
2017-10-22 12:57:14 +0200

    Language update
    Update of default language to English

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div class="site-info"></div>
 </footer>
 </div>
-<a href="#" rel="nofollow" id="totop"><?php echo __('Till toppen', 'wktheme'); ?></a>
+<a href="#" rel="nofollow" id="totop"><?php echo __('To Top', 'wktheme'); ?></a>
 <?php wp_footer(); ?>
 </body>
 </html>
